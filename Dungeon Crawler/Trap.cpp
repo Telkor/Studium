49f1be44b7ed724b512becac9f4bc6ac19eaf676
Telkor <itelkor@hotmail.de>
2017-05-29 20:33:53 +0200

    Add files via upload

--- Dungeon Crawler/Trap.cpp
+++ Dungeon Crawler/Trap.cpp
@@ -14,6 +14,10 @@ bool Trap::setTriggered(bool m_triggered){
     triggered = m_triggered;
 }
 
+bool Trap::getTriggered(){
+    return triggered;
+}
+
 void Trap::print(){
     
     if(triggered == true){
@@ -22,4 +26,31 @@ void Trap::print(){
     else{
         cout << ".";
     }
+    
+    
 }
+
+void Trap::onEnter(Character* c, Tile* fromTile){
+    
+    if(getTriggered() == false){
+        fromTile->setCharacter(nullptr);
+        setCharacter(c);
+        c->getMaxHP() - 20;
+        setTriggered(true);
+    }
+    
+    else{
+        fromTile->setCharacter(nullptr);
+        setCharacter(c);
+        setTriggered(true);
+    }
+    
+    
+    
+}
+
+
+void Trap::onLeave(Tile* toTile){
+    
+    toTile->onEnter(getCharacter(), this); 
+} 
