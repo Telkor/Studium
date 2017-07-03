050faeed1c681c88c0c338a426880ea0bc7b14b4
Telkor <itelkor@hotmail.de>
2017-07-03 14:30:20 +0200

    Delete Controller.cpp

--- Controller.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <vector>
-#include "Controller.h"
-
-Controller::Controller(Character* m_character){
-    character = m_character;
-}
