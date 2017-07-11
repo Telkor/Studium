9a79d95699d1131547a2de5b09d22ffae4497288
Telkor <itelkor@hotmail.de>
2017-07-11 22:56:47 +0200

    Update Character.cpp

--- Dungeon Crawler/Character.cpp
+++ Dungeon Crawler/Character.cpp
@@ -17,11 +17,7 @@ Character::Character(char zeichenart, int m_strength, int m_stamina, Controller*
     stamina = m_stamina;
     hitpoints = getMaxHP();
     controller = m_controller;
-//    stat = new StationaryController(this);
-//    cons = new ConsoleController(this);
-//    att = new AttackController(this);
-//    isKI = m_isKI;
-//    isAtt = m_isAtt;
+
     
 }  void Character::setController(DungeonMap* map, Character* c) {
     controller->setDm(map);
@@ -29,31 +25,9 @@ Character::Character(char zeichenart, int m_strength, int m_stamina, Controller*
 }
 
 char Character::move(){
-//    if (isKI == true && isAtt == false)
-//        return stat->move();
-//    else if (isKI == true && isAtt == true)
-//        return att->move();
-//    else 
-//        return cons->move();
+
    
     controller->move();
- 
-    /*
-    char c;
-    bool exit = false;
-    
-    while (!exit){
-     
-    cin >> c;
-        
-    switch(c) {
-        case 'w': return 'w';
-        case 'a': return 'a';
-        case 's': return 's';
-        case 'd': return 'd';
-        default : return 0;
-    }    
-    }  */
 }
 
 char Character::getZeichen() const{
@@ -80,7 +54,6 @@ void Character::addItem(Item* m_items){
 
 int Character::getStrength(){
     int m_strength = strength;
-    //cout << "Itemanzahl: " << items.size() << "\n";
     for(int i = 0; i < items.size(); i++){
        m_strength += items.at(i)->modifyStrength(strength); 
        
