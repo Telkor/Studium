6679d687e178c22c21ec775323d8497e1e69cfb7
Telkor <itelkor@hotmail.de>
2018-05-20 18:35:40 +0200

    Add files via upload

---
+++ Pizzaservice/jscript.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*var margherita = document.getElementById("margherita");
+margherita = 4;
+*/
+
+/*
+function preis_berechnen(preis){
+    var zahl = 2;    
+    preis = preis + zahl;
+    window.alert("test");
+             return preis2;
+         }
+
+/*document.getElementById( "margherita" ).onclick = preis_berechnen(preis) {
+               preis = preis + 4;
+             return preis;
+};*/
