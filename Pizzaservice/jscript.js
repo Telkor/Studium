046822af6d2b745651921b11e8e5e4eed9250f9e
Telkor <itelkor@hotmail.de>
2018-06-04 03:13:54 +0200

    Add files via upload

--- Pizzaservice/jscript.js
+++ Pizzaservice/jscript.js
@@ -1,18 +1,53 @@
-'use strict';
-
-/*var margherita = document.getElementById("margherita");
-margherita = 4;
-*/
-
 /*
-function preis_berechnen(preis){
-    var zahl = 2;    
-    preis = preis + zahl;
-    window.alert("test");
-             return preis2;
-         }
+"use strict";
 
-/*document.getElementById( "margherita" ).onclick = preis_berechnen(preis) {
-               preis = preis + 4;
-             return preis;
-};*/
+  var preis = 5;
+        var preis2 = 3;
+        var warenkorbPreis = 0;
+             
+        function addHawaii(){
+            var pizzaPreis = 5;
+            warenkorbPreis += pizzaPreis;
+            document.getElementById("preisausgeben").value = warenkorbPreis;
+        };
+             
+        function addMargherita(){
+            var pizzaPreis = 4;
+            warenkorbPreis += pizzaPreis;
+            document.getElementById("preisausgeben").value = warenkorbPreis;
+            
+        }
+        
+        
+        function addSalami(){
+            var pizzaPreis = 4.5;
+            warenkorbPreis += pizzaPreis;
+            document.getElementById("preisausgeben").value = warenkorbPreis;
+        }
+        
+                function addMargheritaOption(){
+            var x = document.getElementById("Warenkorb");
+            var option = document.createElement("option");
+            option.text = "Magherita Pizza";
+            x.add(option);
+        }
+        
+                function addSalamiOption(){
+            var x = document.getElementById("Warenkorb");
+            var option = document.createElement("option");
+            option.text = "Salami Pizza";
+            x.add(option);
+        }
+        
+                function addHawaiiOption(){
+            var x = document.getElementById("Warenkorb");
+            var option = document.createElement("option");
+            option.text = "Hawaii Pizza";
+            x.add(option);
+        }
+        
+        
+        function preis_berechnen(preis){  
+            var zahl = 3;
+            document.getElementById("preisausgeben").value = preis + zahl;   
+        };        
